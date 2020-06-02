c5a68f308cadd50c4533d84756495d4157999d0e
Tylre Collins <tyler@nhlp.com>
2020-06-02 09:30:47 -0600

    service worker template

--- public/db.js
+++ public/db.js
@@ -1,112 +1,65 @@
-loadPosts();
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
-function createEl(htmlString = "", className) {
-  const el = document.createElement(htmlString);
-  if (className) {
-    el.setAttribute("class", className);
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
   }
-  return el;
+};
+
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
 }
 
-function loadPosts() {
-  fetch("/api/posts")
-    .then(res => res.json())
-    .then(data => createCards(data))
-    .catch(err => console.log(err));
-}
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
-function createCards(data) {
-  const container = document.getElementsByClassName("container")[0];
-  container.innerHTML = `<h4 class="text-center mt-2" id="status"></h4>`;
-  let lastRow;
-  const row = createEl("div", "row");
-
-  return data.forEach(function(post, index) {
-    const col = createEl("div", "col-md-4 mt-4");
-    col.appendChild(createCard(post));
-    if (index % 3 === 0) {
-      row.appendChild(col);
-      container.appendChild(row);
-      lastRow = row;
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {        
+        return response.json();
+      })
+      .then(() => {
+        // delete records if successful
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
     }
-
-    return lastRow.appendChild(col);
-  });
+  };
 }
 
-function createCard(post) {
-  const card = createEl("div", "card");
-
-  const imageContainer = createEl("div", "card__image-container");
-  const img = createEl("img", "card-img-top card__image--cover");
-  img.setAttribute("src", `/assets/images/${post.title}-icon.png`);
-  img.setAttribute("alt", post.title);
-  imageContainer.appendChild(img);
-  
-  const cardBody = createEl("div", "card-body");
-  const cardText = createEl("h3", "card-text font-weight-bold mt-2");
-  cardText.innerText = `${post.title}`;
-
-  const cardDescription = createEl("p", "card-text font-weight-bold mt-2");
-  cardDescription.innerText = `${post.description}`;
-
-  const likeButton = createEl("button", "btn");
-  likeButton.setAttribute("id", post._id)
-  likeButton.setAttribute("data-likes", post.likes)
-  likeButton.innerHTML = `<i class="fa fa-thumbs-up" aria-hidden="true"></i> Like`;
-  likeButton.addEventListener("click", incrementLikes)
-
-  const likeText = createEl("div", "card-text text-center mt-5");
-  likeText.setAttribute("id", `likes-count-${post._id}`);
-  likeText.innerText =`Likes: ${post.likes}`;
-
-  cardBody.appendChild(imageContainer);
-  cardBody.appendChild(cardText);
-  cardBody.appendChild(cardDescription);
-  cardBody.appendChild(likeButton);
-  cardBody.appendChild(likeText);
-
-  card.appendChild(cardBody);
-
-  return card;
-}
-
-function incrementLikes(event) {
-  const statusEl = document.querySelector("#status")
-
-  const id = event.currentTarget.getAttribute("id");
-  const oldLikes = parseFloat(event.currentTarget.getAttribute("data-likes"));
-  const likes = oldLikes + 1;
-
-  event.currentTarget.setAttribute("data-likes", likes);
-
-  statusEl.innerText = "";
-
-  incrementLikesRequest(id, likes)
-    .then(() => {
-      statusEl.innerText = "Save successful!"
-      updateLikesDisplay(id, likes, true)
-    })
-    .catch(() => {
-      statusEl.innerText = "Sorry, your 'like' cannot be recorded while you are offline."
-      updateLikesDisplay(id, likes, false)
-    });
-}
-
-function incrementLikesRequest(id, likes) {
-  return fetch(`/api/posts/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({ likes }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-}
-
-function updateLikesDisplay(id, likes, saved) {
-  const likesCount = document.querySelector(`#likes-count-${id}`);
-  likesCount.innerText = `Likes: ${likes}`;
-  if(!saved) {
-    likesCount.innerText += " (not saved)";
-  }
-}
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,15 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/assets/css/style.css",
-    "/assets/js/loadPosts.js",
-    "/assets/images/Angular-icon.png",
-    "/assets/images/React-icon.png",
-    "/assets/images/Vue.js-icon.png",
-    "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
   ];
   
-  const CACHE_NAME = "static-cache-v2";
+  const CACHE_NAME = "static-cache-v1";
   const DATA_CACHE_NAME = "data-cache-v1";
   
   // install
@@ -75,13 +66,17 @@ const FILES_TO_CACHE = [
       return;
     }
   
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
-      })
-    );
-  });
-  
+    event.respondWith(
+        fetch(event.request).catch(function() {
+          return caches.match(event.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (event.request.headers.get("accept").includes("text/html")) {
+              // return the cached home page for all requests for html pages
+              return caches.match("/");
+            }
+          });
+        })
+      );
+    });
   
